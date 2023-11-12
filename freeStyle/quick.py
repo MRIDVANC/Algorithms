0d3da97c5f915658da720012a87fb43107f43939
MRIDVANC <24781973+MRIDVANC@users.noreply.github.com>
2023-11-12 14:11:57 +0300

    quick sort

---
+++ freeStyle/quick.py
@@ -0,0 +1,18 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        less_than_pivot = [x for x in arr[1:] if x <= pivot]
+        greater_than_pivot = [x for x in arr[1:] if x > pivot]
+        return quick_sort(less_than_pivot) + [pivot] + quick_sort(greater_than_pivot)
+
+# Kullanıcıdan bir liste alalım
+sayilar = input("Sıralanacak sayıları boşluk bırakarak girin: ")
+sayi_listesi = list(map(int, sayilar.split()))
+
+# Quick sort uygula
+siralanmis_liste = quick_sort(sayi_listesi)
+
+# Sonucu göster
+print("Sıralanmış liste:", siralanmis_liste)
