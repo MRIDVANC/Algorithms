6268880093a6d3e02299037f812555415014d56e
mridvanc <24781973+MRIDVANC@users.noreply.github.com>
2023-12-25 07:03:32 +0300

    sS

--- freeStyle/insertionSort.py
+++ freeStyle/insertionSort.py
@@ -1,27 +1,18 @@
-def insertion_sort(in_array):
-    for i in range(1, len(in_array)):
-        current_value = in_array[i]
-        position = i
+karma = [9, 5, 6, 8, 47, 2, 3, -1, 24, 7, -6, 8, 5, 1, 34, -5]
 
-        while position > 0 and in_array[position - 1] > current_value:
-            in_array[position] = in_array[position - 1]
-            position = position - 1
+n = len(karma)
 
-        in_array[position] = current_value
 
-# Kullanıcıdan kaç adet sayı almak istediğini sor
-num_of_numbers = int(input("Kaç adet sayı gireceksiniz: "))
+def insertionSort():
+    for i in range(1, n):
+        key = karma[i]
+        j = i - 1
+        while j >= 0 and karma[j] > key:
+            karma[j + 1] = karma[j]
+            j = j - 1
+        karma[j + 1] = key
+    return karma
 
-dizi = []
-for _ in range(num_of_numbers):
-    try:
-        sayi = int(input("Sayıyı giriniz: "))
-        dizi.append(sayi)
-    except ValueError:
-        print("Hatalı giriş. Sayı harici bir karakter girdiniz. Sayı girişine devam ediliyor.")
 
-# Insertion Sort ile sıralama
-insertion_sort(dizi)
-
-print("Sıralanmış Dizi:")
-print(dizi)
+insertionSort()
+print(karma)
